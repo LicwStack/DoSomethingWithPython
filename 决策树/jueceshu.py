a48c24cca35f254cae4ef8f1c89a625fd0b3be1d
lichunwei <LicwStack@163.com>
2018-10-08 14:36:15 +0800

    决策树.

---
+++ 决策树/jueceshu.py
@@ -0,0 +1,36 @@
+from math import log
+
+
+def calcShannonEnt(dataset):
+    numEntries = len(dataset)
+    labelCounts = {}
+    for featVec in dataset:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries  # 事件发生的概率
+        shannonEnt -= prob * log(prob, 2)
+
+    return shannonEnt
+
+
+dataset = [[1, 1, 'maybe'],
+           [1, 1, 'yes'],
+           [1, 0, 'no'],
+           [0, 1, 'no'],
+           [0, 1, 'no']]
+
+print(calcShannonEnt(dataset))
+
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
